cfeb695966cdc802f438fc19fe88175fa90d1195
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-04-10 18:08:40 +0500

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import { AiFillAccountBook } from "react-icons/ai"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element=""></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App
